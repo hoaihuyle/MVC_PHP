23ad3b83d95d4604dbc4bae2002b93357f154ec7
= <=>
2020-03-13 12:59:11 +0700

    indexhome

--- Views/includes/_header.php
+++ Views/includes/_header.php
@@ -5,21 +5,21 @@
    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
    <!-- /Added by HTTrack -->
    <head>
-      <link rel="shortcut icon" href="front/upload/logo/icon_logo.png"/>
-      <link rel="stylesheet" href="front/asset/site/js/slider/bxslider.css">
-      <link rel="stylesheet" href="front/asset/site/css/fontawesome/css/all.css">
-      <link rel="stylesheet" href="front/asset/site/css/bootstrap.min.css">
-      <script src="front/asset/site/js/jquery-3.2.1.min.js"></script> 
-      <link rel="stylesheet" href="front/asset/site/css/thanh.css">
-      <link rel="stylesheet" href="front/asset/site/css/owl.carousel.min.css">
-      <link rel="stylesheet" href="front/asset/site/css/owl.theme.default.min.css">
-      <link rel="stylesheet" href="front/asset/site/css/stylelist13860.css">
-      <script src="front/asset/site/js/bootstrap.min.js"></script> 
-      <script src="front/asset/site/js/slider/jquery.bxslider.min.js"></script> 
-      <script src="front/asset/site/magic/jquery.magic.js"></script> 
-      <script src="front/asset/site/js/jquery-ui.min.js"></script> 
-      <script src="front/asset/site/js/owl.carousel.min.js"></script> 
-      <script src="front/asset/site/js/thanh.js"></script>  
+      <link rel="shortcut icon" href="lib/front/upload/logo/icon_logo.png"/>
+      <link rel="stylesheet" href="lib/front/asset/site/js/slider/bxslider.css">
+      <link rel="stylesheet" href="lib/front/asset/site/css/fontawesome/css/all.css">
+      <link rel="stylesheet" href="lib/front/asset/site/css/bootstrap.min.css">
+      <script src="lib/front/asset/site/js/jquery-3.2.1.min.js"></script> 
+      <link rel="stylesheet" href="lib/front/asset/site/css/thanh.css">
+      <link rel="stylesheet" href="lib/front/asset/site/css/owl.carousel.min.css">
+      <link rel="stylesheet" href="lib/front/asset/site/css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="lib/front/asset/site/css/stylelist13860.css">
+      <script src="lib/front/asset/site/js/bootstrap.min.js"></script> 
+      <script src="lib/front/asset/site/js/slider/jquery.bxslider.min.js"></script> 
+      <script src="lib/front/asset/site/magic/jquery.magic.js"></script> 
+      <script src="lib/front/asset/site/js/jquery-ui.min.js"></script> 
+      <script src="lib/front/asset/site/js/owl.carousel.min.js"></script> 
+      <script src="lib/front/asset/site/js/thanh.js"></script>  
       <meta http-equiv="content-language" content="vi" />
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
