4588ef9bd21aaeeac371085a37966324323c87b2
hoaihuylenguyen <hoaihuylenguyen@gmail.com>
2020-03-14 23:59:39 +0700

    #hide function filter admin/products/index.php

--- Views/admin/products/index.php
+++ Views/admin/products/index.php
@@ -25,7 +25,7 @@
                         <?php } ?>
                         </select>
                     </div>
-                    <div class='col-md-5'>
+                    <!-- <div class='col-md-5'>
                         <div class="form-group">
                         <div class="input-group date" id="datetimepicker7" data-target-input="nearest">
                                 <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker7"/>
@@ -44,7 +44,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="col-md-2">
                         <div class="form-group" id="filterBtnTime">
                             <button class="btn btn-primary"> Lọc </button>
