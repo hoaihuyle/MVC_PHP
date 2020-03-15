d3761269cfc1925d4aa94be61422b0e0a35d3045
hoaihuylenguyen <hoaihuylenguyen@gmail.com>
2020-03-15 19:32:55 +0700

    # add db and ckeditor 5

---
+++ Models/DAO/menuDAO copy.php
@@ -0,0 +1,40 @@
+<?php
+class MenuDAO extends Model
+{
+    
+    public function fetchAll($db){
+        return $db->fetchAll('menu');
+    }
+
+    public function fetchByCol($db,$id){
+        return $db->fetchByCol('menu','id_menu',$id);
+    }
+
+    function fetchByColOther($db){
+        try {
+            //code...
+            return $db->fetchByColOther('menu','status','1'); 
+        } catch (\Throwable $th) {
+            //throw $th;
+            return $err = 'Try late!!';
+        }
+         
+    }
+
+    public function update($db, $id, $data){
+
+        return $db->update('menu', $data, array('id_menu'=>$id));
+    }
+
+    public function insert($db, $data){
+
+        return $db->insert('menu', $data ); 
+    }
+
+    public function delete($db, $id){
+
+        return $db->delete('menu', $id, 'id_menu'); 
+    }
+ 
+}
+?>
